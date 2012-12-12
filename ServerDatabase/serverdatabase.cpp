24f38d3159cb575fbdcb26ad9cc739a43687a4db
gilles fabre <gilles.fabre@intel.com>
2012-12-12 15:31:32 +0100

    can't change connection time out from SION! server, must be done at the
    MySQL server level. Code rolled back.

--- ServerDatabase/serverdatabase.cpp
+++ ServerDatabase/serverdatabase.cpp
@@ -74,9 +74,6 @@ ServerDatabase::ServerDatabase() {
         }
         m_db = QSqlDatabase::addDatabase(sqlDriver);
 
-        // don't disconnect even if there's no interaction with the db server
-        m_db.setConnectOptions("MYSQL_OPT_CONNECT_TIMEOUT=0");
-
         // create tables if non existing
         createTables();
     }
